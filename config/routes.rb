c338fa7ca8e6d2e8b80a11e9eb5428dad1536a8d
FelixAGonzales <FelixAGonzales93@gmail.com>
2024-09-01 12:45:11 -0400

    add contact details

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  get "a_contact" => "contacts#contact_details"
   # Defines the root path route ("/")
   # root "posts#index"
 end
